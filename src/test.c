d59d132f27adb8f65da526a6b257eea2cebb47e4
Lee <hansonlee.hk@outlook.com>
2025-08-21 20:01:27 +0800

    鹏哥C语言27

--- src/test.c
+++ src/test.c
@@ -7,6 +7,8 @@ void test() {
 }
 */
 
+/*
+//一起学C语言。。。
 void test(int argc, char **argv){
 //	unsigned int AHB1ENR = 0x00;
 //	unsigned int b = 0x01;
@@ -20,18 +22,24 @@ void test(int argc, char **argv){
 	b = *((char *)&a + 2);
 	printf("b = 0x%x\n", b);	
 	
+//	int num = 1;
+//	printf("num = %d\n", num);	
+*/
+
+
+void test(){
+	int num = 0;
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	for (int i = 0; i < 100; i++)
+	{
+		if (i % 2 == 1)
+		{
+			printf("%d\t", i);
+		}
+	}
+	printf("\n");
+
+
 
 	return 0;
 
